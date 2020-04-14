ed7106b398d8b47d08d38cfc910d50ee7f7fb217
MR.D <mayurdhameliya1@gmail.com>
2020-04-14 21:10:49 +0530

    Add channels images in assets

--- src/app/mock-channels.ts
+++ src/app/mock-channels.ts
@@ -4,31 +4,37 @@ export const CHANNELS: ChannelsFields[] = [
     {
         channelId: 11,
         channelName: 'CNBC TV18',
+        channelImg: '1',
         channelPrice: 19
     },
     {
         channelId: 12,
         channelName: 'Univision',
+        channelImg: '1',
         channelPrice: 9
     },
     {
         channelId: 13,
         channelName: 'Fox News',
+        channelImg: '1',
         channelPrice: 7
     },
     {
         channelId: 14,
         channelName: 'CBS',
+        channelImg: '1',
         channelPrice: 14
     },
     {
         channelId: 15,
         channelName: 'Al Jazeera',
+        channelImg: '1',
         channelPrice: 19
     },
     {
         channelId: 16,
         channelName: 'Disney XD',
+        channelImg: '1',
         channelPrice: 19
     },
 ];
