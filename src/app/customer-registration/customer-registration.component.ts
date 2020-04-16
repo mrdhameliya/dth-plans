f2142e23c6d8a309137f681c587013d71863755f
MR.D <mayurdhameliya1@gmail.com>
2020-04-16 20:45:13 +0530

    fix(customer-registration): add security questions and add validation in all fields and also add password match validator file

--- src/app/customer-registration/customer-registration.component.ts
+++ src/app/customer-registration/customer-registration.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { SecurityQuestion } from '../models/security-questions.model';
+import { MustMatch } from '../custom-helper/password-match.validator';
 
 @Component({
-  selector: 'app-customer-registration',
+  selector: 'dth-customer-registration',
   templateUrl: './customer-registration.component.html',
   styleUrls: ['./customer-registration.component.scss']
 })
 export class CustomerRegistrationComponent implements OnInit {
 
+  securityQuestions: SecurityQuestion[] = [
+    { id: 1, securityQuestion: 'First car you own' },
+    { id: 2, securityQuestion: 'Your first school name' },
+    { id: 3, securityQuestion: 'Your pet name' },
+    { id: 4, securityQuestion: 'Your favourite crickter' }
+  ];
+
   constructor(private fb: FormBuilder) { }
 
   customerRegistrationForm = this.fb.group({
-    vcNumber: ['', Validators.required],
-    mobileNumber: [''],
-    email: [''],
-    alternateMobileNumber: [''],
-    password: [''],
-    confirmPassword: [''],
-    securityQuestion: [''],
-    answer: ['']
+    vcNumber: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(12)]],
+    mobileNumber: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+    email: ['', [Validators.required, Validators.email]],
+    alternateMobileNumber: ['', [Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', Validators.required],
+    securityQuestion: ['', [Validators.required]],
+    answer: ['', [Validators.required, Validators.minLength(2)]]
+  }, {
+    validator: MustMatch('password', 'confirmPassword')
   });
 
   ngOnInit(): void {
@@ -27,4 +38,9 @@ export class CustomerRegistrationComponent implements OnInit {
   onSubmit() {
     console.warn(this.customerRegistrationForm.value);
   }
+
+  get f() {
+    return this.customerRegistrationForm.controls;
+  }
+
 }
---
+++ src/app/models/security-questions.model.ts
@@ -0,0 +1,4 @@
+export class SecurityQuestion {
+    id: number;
+    securityQuestion: string;
+}
