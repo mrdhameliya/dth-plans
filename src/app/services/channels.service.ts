dfd82ef00024a19fb6612aa9e3b1798e9ee7609d
MR.D <mayurdhameliya1@gmail.com>
2020-04-20 19:38:46 +0530

    add into services folder add save and handleError method set payload

---
+++ src/app/services/channels.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ChannelsFields } from '../channels-fields';
+import { Observable, throwError } from 'rxjs';
+import { CHANNELS } from '../mock-channels';
+import { PlansFields } from '../plans-fields';
+import { PLANS } from '../mock-plans';
+import { Customer } from '../models/customer.model';
+
+
+@Injectable()
+export class ChannelsService {
+
+    private channelsUrl = 'url';
+
+    constructor(
+        private http: HttpClient) { }
+
+    getChannels(): ChannelsFields[] {
+        return CHANNELS;
+    }
+
+    getPlans(): PlansFields[] {
+        return PLANS;
+    }
+
+    save(customer: Customer): Observable<Customer> {
+        if (customer.customerId == null) {
+            return this.http
+                .post<Customer>('this.channelsUrl', customer, {
+                    headers: new HttpHeaders({
+                        'Content-type': 'application/json'
+                    })
+                });
+        }
+    }
+
+    handleError(error: any) {
+        console.error(error);
+        return throwError(error);
+      }
+}
