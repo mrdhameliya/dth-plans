f96dea1784f4e5c337ad5ce86a0f30b4c511cb9c
MR.D <mayurdhameliya1@gmail.com>
2020-04-22 20:29:43 +0530

    fix(instant-recharge): create complete instant recharge screen

--- src/app/instant-recharge/instant-recharge.component.ts
+++ src/app/instant-recharge/instant-recharge.component.ts
@@ -8,11 +8,48 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class InstantRechargeComponent implements OnInit {
 
+  public click1 = 0;
+  public click2 = 0;
+  public block1 = true;
+  public block2 = false;
+  public block3 = false;
+
+
   vcNumber = new FormControl('', [Validators.required, Validators.minLength(12), Validators.maxLength(12)]);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickFun1() {
+    this.click1 = 1;
+    this.block1 = !this.block1;
+    this.block2 = !this.block2;
+  }
+
+  toggle1() {
+    if (this.click1 === 1) {
+      this.block1 = !this.block1;
+    }
+  }
+
+  clickFun2() {
+    this.click2 = 1;
+    this.block2 = !this.block2;
+    this.block3 = !this.block3;
+  }
+
+  toggle2() {
+    if (this.click1 === 1) {
+      this.block2 = !this.block2;
+    }
+  }
+
+  toggle3() {
+    if (this.click2 === 1) {
+      this.block3 = !this.block3;
+    }
   }
 
 }
