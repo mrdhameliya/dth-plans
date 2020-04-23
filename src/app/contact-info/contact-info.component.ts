b603bc9b45869da9ebaa9f0ec581a73d51be6fd9
MR.D <mayurdhameliya1@gmail.com>
2020-04-23 19:45:05 +0530

    feat(contact-info): create component for contact details using table

---
+++ src/app/contact-info/contact-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dth-contact-info',
+  templateUrl: './contact-info.component.html',
+  styleUrls: ['./contact-info.component.scss']
+})
+export class ContactInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
