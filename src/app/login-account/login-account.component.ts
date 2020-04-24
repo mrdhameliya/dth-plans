4687372f5def0c048002c82058332cea8364a9e1
MR.D <mayurdhameliya1@gmail.com>
2020-04-24 19:41:06 +0530

    fix(login-account): change validation real time to submit button click

--- src/app/login-account/login-account.component.ts
+++ src/app/login-account/login-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dth-login-account',
@@ -8,22 +9,34 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginAccountComponent implements OnInit {
 
+  submitted = false;
+
   loginForm = this.fb.group({
     vcNumber: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(12)]],
     mobileNumber: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
     otp: ['', [Validators.required]]
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
-  }
-
-  onSubmit() {
-    console.warn(this.loginForm.value);
   }
 
   get f() {
     return this.loginForm.controls;
   }
+  onSubmit() {
+    console.warn(this.loginForm.value);
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    alert('Login Successfully!');
+    this.route.navigate(['/plans-list']);
+  }
+
 }
+
