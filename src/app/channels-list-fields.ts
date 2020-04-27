9fe3cbef02cff2ab379787d68105e17bd96e6675
MR.D <mayurdhameliya1@gmail.com>
2020-04-27 20:50:21 +0530

    create mock data for category of channel and channels list

---
+++ src/app/channels-list-fields.ts
@@ -0,0 +1,11 @@
+export interface CategoryFields {
+    categoryId: number;
+    channelCategoryName: string;
+}
+
+export interface CategoryChannelFields {
+    categoryId: number;
+    channelId: number;
+    channelName: string;
+    channelImg: any;
+}
---
+++ src/app/mock-channels-list.ts
@@ -0,0 +1,193 @@
+import { CategoryFields, CategoryChannelFields } from './channels-list-fields';
+
+export const CATEGORYLIST: CategoryFields[] = [
+    {
+        categoryId: 1,
+        channelCategoryName: 'DEVOTIONAL',
+    },
+    {
+        categoryId: 2,
+        channelCategoryName: 'GENERAL ENTERTAINMENT',
+    },
+    {
+        categoryId: 3,
+        channelCategoryName: 'INFOTAINMENT',
+    },
+    {
+        categoryId: 4,
+        channelCategoryName: 'KIDS',
+    },
+    {
+        categoryId: 5,
+        channelCategoryName: 'LIFESTYLE & FASHION',
+    },
+    {
+        categoryId: 6,
+        channelCategoryName: 'MOVIES',
+    },
+    {
+        categoryId: 7,
+        channelCategoryName: 'MUSIC',
+    },
+    {
+        categoryId: 8,
+        channelCategoryName: 'NEWS',
+    },
+    {
+        categoryId: 9,
+        channelCategoryName: 'OTHER',
+    },
+    {
+        categoryId: 10,
+        channelCategoryName: 'SPORTS',
+    },
+];
+
+export const CHANNELSLIST: CategoryChannelFields[] = [
+    {
+        categoryId: 1,
+        channelId: 101,
+        channelName: 'Aastha',
+        channelImg: '../../assets/Channels/aastha.png'
+    },
+    {
+        categoryId: 1,
+        channelId: 102,
+        channelName: 'Arihant',
+        channelImg: '../../assets/Channels/arihant-tv.png'
+    },
+    {
+        categoryId: 2,
+        channelId: 103,
+        channelName: 'Colors Rishtey',
+        channelImg: '../../assets/Channels/colors-rishtey.png'
+    },
+    {
+        categoryId: 2,
+        channelId: 104,
+        channelName: 'DD National',
+        channelImg: '../../assets/Channels/dd-national.png'
+    },
+    {
+        categoryId: 2,
+        channelId: 105,
+        channelName: 'Zee Anmol',
+        channelImg: '../../assets/Channels/zee-anmol.png'
+    },
+
+    {
+        categoryId: 3,
+        channelId: 106,
+        channelName: 'DD Kisan',
+        channelImg: '../../assets/Channels/dd-kisan.png'
+    },
+    {
+        categoryId: 3,
+        channelId: 107,
+        channelName: 'History TV 18',
+        channelImg: '../../assets/Channels/history-tv-18.png'
+    },
+    {
+        categoryId: 3,
+        channelId: 108,
+        channelName: 'Discovery',
+        channelImg: '../../assets/Channels/discovery.png'
+    },
+
+    {
+        categoryId: 4,
+        channelId: 109,
+        channelName: 'Cartoon Network',
+        channelImg: '../../assets/Channels/cartoon-network.png'
+    },
+    {
+        categoryId: 4,
+        channelId: 110,
+        channelName: 'Pogo',
+        channelImg: '../../assets/Channels/pogo.png'
+    },
+
+    {
+        categoryId: 5,
+        channelId: 111,
+        channelName: 'Living Foodz',
+        channelImg: '../../assets/Channels/living-foodz.png'
+    },
+
+    {
+        categoryId: 6,
+        channelId: 112,
+        channelName: 'pictures',
+        channelImg: '../../assets/Channels/andpictures.png'
+    },
+    {
+        categoryId: 6,
+        channelId: 113,
+        channelName: 'B4U Kadak',
+        channelImg: '../../assets/Channels/b4u-kadak.png'
+    },
+    {
+        categoryId: 6,
+        channelId: 114,
+        channelName: 'UTV Action',
+        channelImg: '../../assets/Channels/utv-action.png'
+    },
+
+    {
+        categoryId: 7,
+        channelId: 115,
+        channelName: '9X Jhakaas',
+        channelImg: '../../assets/Channels/9x-jhakaas.png'
+    },
+    {
+        categoryId: 7,
+        channelId: 116,
+        channelName: 'Zing',
+        channelImg: '../../assets/Channels/zing.png'
+    },
+    {
+        categoryId: 7,
+        channelId: 117,
+        channelName: 'MTV',
+        channelImg: '../../assets/Channels/mtv.png'
+    },
+
+    {
+        categoryId: 8,
+        channelId: 118,
+        channelName: 'Aaj Tak',
+        channelImg: '../../assets/Channels/aaj-tak.png'
+    },
+    {
+        categoryId: 8,
+        channelId: 119,
+        channelName: 'India TV',
+        channelImg: '../../assets/Channels/india-tv.png'
+    },
+    {
+        categoryId: 8,
+        channelId: 120,
+        channelName: 'NDTV India',
+        channelImg: '../../assets/Channels/ndtv-india.png'
+    },
+
+    {
+        categoryId: 9,
+        channelId: 121,
+        channelName: 'Watcho',
+        channelImg: '../../assets/Channels/watcho.png'
+    },
+
+    {
+        categoryId: 10,
+        channelId: 122,
+        channelName: 'Star Sports 1 Hindi',
+        channelImg: '../../assets/Channels/star-sports-1-hindi.png'
+    },
+    {
+        categoryId: 10,
+        channelId: 123,
+        channelName: 'Sony Ten 3',
+        channelImg: '../../assets/Channels/sony-ten-3.png'
+    },
+];
