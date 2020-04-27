8bd5000b2e12436bb7459ffa8e7c64a3dc1188cc
MR.D <mayurdhameliya1@gmail.com>
2020-04-27 20:51:20 +0530

    add method for get mock data of channel category and channel list

--- src/app/services/channels.service.ts
+++ src/app/services/channels.service.ts
@@ -7,6 +7,8 @@ import { PlansFields } from '../plans-fields';
 import { PLANS } from '../mock-plans';
 import { Customer } from '../models/customer.model';
 import { CUSTOMER } from '../mock-customer';
+import { CHANNELSLIST, CATEGORYLIST } from '../mock-channels-list';
+import { CategoryFields, CategoryChannelFields } from '../channels-list-fields';
 
 
 @Injectable()
@@ -29,10 +31,18 @@ export class ChannelsService {
         return CUSTOMER;
     }
 
+    getCategoryList(): CategoryFields[] {
+        return CATEGORYLIST;
+    }
+
+    getChannelsList(): CategoryChannelFields[] {
+        return CHANNELSLIST;
+    }
+
     save(customer: Customer): Observable<Customer> {
         if (customer.customerId == null) {
             return this.http
-                .post<Customer>('this.channelsUrl', customer, {
+                .post<Customer>(this.channelsUrl, customer, {
                     headers: new HttpHeaders({
                         'Content-type': 'application/json'
                     })
